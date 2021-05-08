ab4bf796e0a7b5104841705dac431e28a7cc062b
Rob Clark <rob@robclark.io>
2021-05-08 03:58:25 -0500

    Several upgrades and optimizations.
    This release contains optimizations for gulpfile.js, includes several upgrades for bundled libraries, and switches to a new Bootstrap navwalker.
    
    Changes:
    * Updated bundled Bootstrap to v5.0.0 (from v5.0.0-beta3)
    * Updated bundled Boostrap Icons to v1.4.1 (from v1.4.0)
    * Updated Jarallax to v1.2.5 (from v1.2.4)
    * The theme now uses AlexWebLab's Bootstrap 5 navwalker (https://github.com/AlexWebLab/bootstrap-5-wordpress-navbar-walker)
    * ( package.json ) Removed uneeded dependencies.
    * ( package.json ) Updated minimum versions for several dependencies: browser-sync 2.26.3 => 2.26.14, gulp 4.0.0 => 4.0.2, gulp-autoprefixer 4.0.0 => 7.0.1, gulp-dart-sass 0.9.1 => 1.0.2, gulp-notify 3.0.0 => 4.0.0, gulp-rename 1.2.2 => 2.0.0, gulp-uglify 3.0.0 => 3.0.2
    * ( gulpfile.js ) Removed uneeded dependencies.
    * ( gulpfile.js ) The serve task no longer reloads on css/scss changes, as they are streamed to the browser via the compile-styles task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,26 @@
 var gulp = require('gulp'),
 sass = require('gulp-dart-sass'),
 autoprefixer = require('gulp-autoprefixer'),
-jshint = require('gulp-jshint'),
 uglify = require('gulp-uglify'),
-imagemin = require('gulp-imagemin'),
 rename = require('gulp-rename'),
 concat = require('gulp-concat'),
 notify = require('gulp-notify'),
-cache = require('gulp-cache'),
 browserSync = require('browser-sync').create(),
-del = require('del'),
-watchSass = require("gulp-watch-sass"),
-cleanCSS = require('gulp-clean-css');
+cleanCSS = require('gulp-clean-css'),
+postcss = require('gulp-postcss'),
+assets  = require('postcss-assets');
+
+gulp.task('scripts', function() {
+    return gulp.src(['inc/bootstrap/js/bootstrap.bundle.js','inc/matchHeight/jquery.matchHeight.js','inc/jarallax/jarallax.js'])
+      .pipe(concat('assets/js/scripts.js'))
+      .pipe(gulp.dest('.'))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(uglify())
+      .pipe(gulp.dest('.'))
+      .pipe(notify({ message: 'Scripts task complete' }));
+});
 
 gulp.task('process-styles', function () {
-	var postcss = require('gulp-postcss');
-	var assets  = require('postcss-assets');
-   
 	return gulp.src(['./theme.css','theme.min.css'])
 	  .pipe(postcss([assets({
 		loadPaths: ['inc/bootstrap-icons/','assets/images/']
@@ -39,34 +43,15 @@ gulp.task('compile-styles', function() {
 gulp.task('styles', gulp.series('compile-styles', 'process-styles'));
 
 gulp.task('serve', function() {
-
     browserSync.init({
         proxy: "method.test:8080"
     });
-
     // Watch .scss files
     gulp.watch(['./**/*.scss', '!./node_modules/', '!./.git/'], gulp.series('compile-styles', 'process-styles'));
-
-    gulp.watch(['./**/*.*', '!./node_modules/', '!./.git/']).on('change', browserSync.reload);
-    
+    gulp.watch(['./**/*.*', '!./node_modules/', '!./.git/', '!./**/*.scss', '!./theme.css', '!./theme.min.css']).on('change', browserSync.reload);
 });
-
-gulp.task('scripts', function() {
-    return gulp.src(['inc/bootstrap/js/bootstrap.bundle.js','inc/matchHeight/jquery.matchHeight.js','inc/jarallax/jarallax.js'])
-      .pipe(jshint('.jshintrc'))
-      .pipe(jshint.reporter('default'))
-      .pipe(concat('assets/js/scripts.js'))
-      .pipe(gulp.dest('.'))
-      .pipe(rename({suffix: '.min'}))
-      .pipe(uglify())
-      .pipe(gulp.dest('.'))
-      .pipe(notify({ message: 'Scripts task complete' }));
-});
-
 
 gulp.task('watch', function() {
-    
       // Watch .scss files
       gulp.watch(['./**/*.scss', '!./node_modules/', '!./.git/'], gulp.series('compile-styles', 'process-styles'));
-    
 });

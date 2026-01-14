533d12d4f6962b44422c987187bf3224dc8013e3
Rob Clark <rob@robclark.io>
2026-01-14 10:21:24 -0600

    Container block fixes

--- lib/blocks/method-container/build/index.asset.php
+++ lib/blocks/method-container/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => 'd20f7d02c99c957ca042');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => 'bd4a595b272b6ef82dee');

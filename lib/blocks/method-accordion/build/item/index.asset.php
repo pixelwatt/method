1984c05b46337ab65ba0a2d5cfbe717d01af4953
Rob Clark <rob@robclark.io>
2026-01-20 05:36:21 -0600

    Added Accordion block

--- lib/blocks/method-accordion/build/item/index.asset.php
+++ lib/blocks/method-accordion/build/item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element'), 'version' => '4ada1552203023d14985');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => '3d135113530f4ce67aa4');
--- lib/blocks/method-accordion/method-accordion.php
+++ lib/blocks/method-accordion/method-accordion.php
@@ -24,13 +24,19 @@ function register_method_accordion_body_block() {
 add_action( 'init', 'register_method_accordion_body_block' );
 
 function render_method_accordion_block( $block_attributes, $content, $block ) {
-
+    return '<div ' . get_block_wrapper_attributes( ['class' => 'method-accordion' ] ) . '>' . do_blocks( $content ) . '</div>';
 }
 
 function render_method_accordion_item_block( $block_attributes, $content, $block ) {
-
+    $itemIndex = $block_attributes['itemIndex'];
+    return '
+        <h2 class="accordion-header"><button class="accordion-button' . ( 1 == $itemIndex ? '' : ' collapsed' ) . '" type="button" aria-expanded="' . ( 1 == $itemIndex ? 'true' : 'false' ) . '" aria-controls="collapse' . $itemIndex . '" data-bs-toggle="collapse" data-bs-target="#collapse' . $itemIndex . '">' . ( method_check_array_key( $block_attributes, 'headline' ) ? $block_attributes['headline'] : 'Accordion Item' ) . '</button></h2>
+        <div ' . get_block_wrapper_attributes( ['class' => 'accordion-item' ] ) . '>
+            ' . do_blocks( $content ) . '
+        </div>
+    ';
 }
 
 function render_method_accordion_body_block( $block_attributes, $content, $block ) {
-
+    return '<div ' . get_block_wrapper_attributes( ['class' => 'accordion-body' ] ) . '>' . do_blocks( $content ) . '</div>';
 }

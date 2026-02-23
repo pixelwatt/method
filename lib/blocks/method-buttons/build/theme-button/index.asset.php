88ceb77293e4b6f36b7591570da3bafcd4633b7f
Rob Clark <rob@robclark.io>
2026-02-23 12:14:53 -0600

    theme-button example viewport width now set to 300px

--- lib/blocks/method-buttons/build/theme-button/index.asset.php
+++ lib/blocks/method-buttons/build/theme-button/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '88572bce0431709a11f8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '6f043bb9e3883d1aed4a');

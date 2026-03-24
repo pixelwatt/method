0693f3ae8d78e6513c3c065b65dff3a13e002992
Rob Clark <rob@robclark.io>
2026-03-23 19:28:19 -0500

    Added ability to set width and max width for containers

--- lib/blocks/method-container/build/index.asset.php
+++ lib/blocks/method-container/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element'), 'version' => '0eb4e857ae8049867b90');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element'), 'version' => '75c732dbc28b209f08f0');
--- lib/blocks/method-container/method-container.php
+++ lib/blocks/method-container/method-container.php
@@ -21,7 +21,7 @@ function render_method_container_block( $block_attributes, $content, $block ) {
 
     $cssargs = array(
         '#' . $methodId => array( 'borderRadius', 'marginLeftNonZero', 'marginRightNonZero', 'margin-top', 'margin-bottom', 'boxShadow' ),
-        '#' . $methodId . ' > .method-container-content' => array( 'color', 'bgColor', 'borderRadius', 'border', 'padding-left', 'padding-right', 'padding-top', 'padding-bottom', 'fontSize', 'lineHeight', 'height', 'minHeight', 'alignItems' ),
+        '#' . $methodId . ' > .method-container-content' => array( 'color', 'bgColor', 'borderRadius', 'border', 'padding-left', 'padding-right', 'padding-top', 'padding-bottom', 'fontSize', 'lineHeight', 'height', 'minHeight', 'width', 'minWidth', 'alignItems' ),
         '#' . $methodId . ' > .method-container-content > .method-container-shade' => array( 'bgShade' ),
         '#' . $methodId . ' a' => array( 'linkColor' )
     );
